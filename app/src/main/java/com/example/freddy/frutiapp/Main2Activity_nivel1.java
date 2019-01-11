ec5225f2902165ac0a2ac365587d2bed71a086dd
freddy <copamamanifreddy@gmail.com>
2019-01-11 12:35:00 -0400

    conexion a sqliteBD

---
+++ app/src/main/java/com/example/freddy/frutiapp/Main2Activity_nivel1.java
@@ -0,0 +1,13 @@
+package com.example.freddy.frutiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity_nivel1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_nivel1);
+    }
+}
--- app/src/main/java/com/example/freddy/frutiapp/MainActivity.java
+++ app/src/main/java/com/example/freddy/frutiapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.freddy.frutiapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,5 +51,51 @@ public class MainActivity extends AppCompatActivity {
             id = getResources().getIdentifier("uva", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
         }
+
+
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "BD", null, 1);
+        SQLiteDatabase BD = admin.getWritableDatabase();
+
+        Cursor consulta = BD.rawQuery(
+                "select * from puntaje where  score = (select max(score)from puntaje )", null);
+
+        if(consulta.moveToFirst()){
+            String temp_nombre = consulta.getString(0);
+            String temp_score = consulta.getString(1);
+            tv_bestScore.setText("Record: " + temp_score + " de " + temp_nombre);
+            BD.close();
+        }else {
+            BD.close();
+        }
+
+        mp = MediaPlayer.create(this, R.raw.alphabet_song);
+        mp.start();
+        mp.setLooping(true);
+    }
+
+    public  void Jugar(View view){
+        String nombre = et_nombre.getText().toString();
+
+        if(!nombre.equals("")){
+            mp.stop();
+            mp.release();
+
+            Intent intent = new Intent(this, Main2Activity_nivel1.class);
+
+            intent.putExtra("jugador", nombre);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Primero debes escribir tu nombre", Toast.LENGTH_SHORT).show();
+
+            et_nombre.requestFocus();
+            InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    @Override
+    public void  onBackPressed(){
+
     }
 }
